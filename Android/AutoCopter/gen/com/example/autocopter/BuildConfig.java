ae792b5555e118b0e6dbca98cb4a5e77c4550ee2
WindAzure <cvbnm0002@yahoo.com.tw>
2014-08-09 15:24:32 +0800

    Add android project

---
+++ Android/AutoCopter/gen/com/example/autocopter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.autocopter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Android/AutoCopter/src/com/example/stable/value/ConstValue.java
@@ -0,0 +1,7 @@
+package com.example.stable.value;
+
+public class ConstValue 
+{
+	public static String LOG_VERBOSE_EXCEPTION_TAG="Exception";
+	public static String LOG_VERBOSE_GCM_TAG="Gcm";
+}
