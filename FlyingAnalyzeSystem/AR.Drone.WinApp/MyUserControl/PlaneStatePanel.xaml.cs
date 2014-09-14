74440a63e963676e1d8a0ef81aad35c64326c45a
WindAzure <cvbnm0002@yahoo.com.tw>
2014-09-14 13:04:22 +0800

    electric finish

--- FlyingAnalyzeSystem/AR.Drone.WinApp/MyUserControl/PlaneStatePanel.xaml.cs
+++ FlyingAnalyzeSystem/AR.Drone.WinApp/MyUserControl/PlaneStatePanel.xaml.cs
@@ -67,5 +67,62 @@ namespace AR.Drone.WinApp.MyUserControl
             }
             Debug.WriteLine(isFirst.ToString());
         }
+
+        public void SetBattery(double rate)
+        {
+            _battery.Width = 100 * rate;
+            Color endColor=GetRelativeColor(_batterySampleBrush.GradientStops, rate);
+            Color middleColor = GetRelativeColor(_batterySampleBrush.GradientStops, rate / 2.0);
+            
+            LinearGradientBrush gradientBrushGroup = new LinearGradientBrush();
+            GradientStop start = new GradientStop();
+            start.Color = Colors.Red;
+            start.Offset = 0;
+            gradientBrushGroup.GradientStops.Add(start);
+
+            GradientStop middle = new GradientStop();
+            middle.Color = middleColor;
+            middle.Offset = 0.5;
+            gradientBrushGroup.GradientStops.Add(middle);
+
+            GradientStop end = new GradientStop();
+            end.Color = endColor;
+            end.Offset = 1;
+            gradientBrushGroup.GradientStops.Add(end);
+
+            _battery.Fill = gradientBrushGroup;
+        }
+
+        private Color GetRelativeColor(GradientStopCollection gsc, double offset)
+        {
+            GradientStop before = gsc.Where(w => w.Offset == gsc.Min(m => m.Offset)).First();
+            GradientStop after = gsc.Where(w => w.Offset == gsc.Max(m => m.Offset)).First();
+
+            foreach (var gs in gsc)
+            {
+                if (gs.Offset < offset && gs.Offset > before.Offset)
+                {
+                    before = gs;
+                }
+                if (gs.Offset > offset && gs.Offset < after.Offset)
+                {
+                    after = gs;
+                }
+            }
+
+            var color = new Color();
+
+            color.ScA = (float)((offset - before.Offset) * (after.Color.ScA - before.Color.ScA) / (after.Offset - before.Offset) + before.Color.ScA);
+            color.ScR = (float)((offset - before.Offset) * (after.Color.ScR - before.Color.ScR) / (after.Offset - before.Offset) + before.Color.ScR);
+            color.ScG = (float)((offset - before.Offset) * (after.Color.ScG - before.Color.ScG) / (after.Offset - before.Offset) + before.Color.ScG);
+            color.ScB = (float)((offset - before.Offset) * (after.Color.ScB - before.Color.ScB) / (after.Offset - before.Offset) + before.Color.ScB);
+
+            return color;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SetBattery(bbb.Value/100.0);
+        }
     }
 }
